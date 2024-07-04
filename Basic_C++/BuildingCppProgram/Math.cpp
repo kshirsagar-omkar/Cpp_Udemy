4dd3c2062da95bc661263038e6f84a04e2010ba1
Omkar21-1 <omkarkshirsagar2005@gmail.com>
2024-07-05 01:25:53 +0530

    Linking Process

---
+++ Basic_C++/BuildingCppProgram/Math.cpp
@@ -0,0 +1,13 @@
+module Math;
+
+int Add(int N, int M)
+{
+	return N + M;
+}
+
+int Subtract(int N, int M)
+{
+	return N - M;
+}
+
+int A {7};
---
+++ Basic_C++/BuildingCppProgram/Math.cxx
@@ -0,0 +1,28 @@
+export module Math;
+
+export int Add(int N, int M);
+
+
+export int Subtract(int N, int M);
+
+
+export int A;
+
+
+
+
+
+
+
+// export module Math;
+
+// export int Add(int N, int M)
+// {
+// 	return N + M;
+// }
+
+
+// export int Subtract(int N, int M)
+// {
+// 	return N - M;
+// }
